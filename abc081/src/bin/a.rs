3826325aec33df075165e72b9bad478ce3a2af51
positoro <positoro@gmail.com>
2023-04-14 02:22:23 +0900

    abc081a

--- abc081/src/bin/a.rs
+++ abc081/src/bin/a.rs
@@ -1,3 +1,15 @@
 fn main() {
-    //    todo!();
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+
+    let strings: &str = input_strings.trim();
+    let strings_iter = strings.chars();
+    let mut counter = 0;
+
+    for character in strings_iter {
+        if character == '1' {
+            counter = counter + 1;
+        }
+    }
+    println!("{}", counter);
 }
