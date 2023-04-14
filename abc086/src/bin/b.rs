cbcef0c13a5efa41393ae4558bcc361ea731f005
positoro <positoro@gmail.com>
2023-04-14 19:23:11 +0900

    b.rs coding.

--- abc086/src/bin/b.rs
+++ abc086/src/bin/b.rs
@@ -1,3 +1,6 @@
 fn main() {
-    todo!();
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    input_strings.retain(|c| !c.is_whitespace());
+    println!("{}", input_strings);
 }
