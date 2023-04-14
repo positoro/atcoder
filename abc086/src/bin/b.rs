35f99506f09d1b2165620014f3369b5174dae361
positoro <positoro@gmail.com>
2023-04-14 19:49:01 +0900

    b.rs

--- abc086/src/bin/b.rs
+++ abc086/src/bin/b.rs
@@ -2,5 +2,13 @@ fn main() {
     let mut input_strings = String::new();
     std::io::stdin().read_line(&mut input_strings).ok();
     input_strings.retain(|c| !c.is_whitespace());
-    println!("{}", input_strings);
+    let number: f32 = input_strings.parse().unwrap();
+    let sqrt_of_number: f32 = number.sqrt();
+    let zyosuu: f32 = number % sqrt_of_number;
+
+    if zyosuu == 0.0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
