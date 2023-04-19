a108223a263ea807307b0dd6f734efd363d57504
positoro <positoro@gmail.com>
2023-04-19 19:43:15 +0900

    a081

--- abc081/src/bin/b.rs
+++ abc081/src/bin/b.rs
@@ -1,3 +1,34 @@
 fn main() {
-    todo!();
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    input_strings.clear();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let strings_vec: Vec<&str> = input_strings.trim().split_whitespace().collect();
+
+    let mut counter = 0;
+    let mut counted_vec: Vec<u32> = Vec::new();
+    let mut next_counted_vec: Vec<u32> = Vec::new();
+
+    for number_char in strings_vec {
+        counted_vec.push(number_char.parse().unwrap());
+    }
+
+    'outer: loop {
+        for number in &counted_vec {
+            if number % 2 == 0 {
+                next_counted_vec.push(*number / 2);
+            } else {
+                break 'outer;
+            }
+        }
+
+        counted_vec.clear();
+        for number in &next_counted_vec {
+            counted_vec.push(*number);
+        }
+        next_counted_vec.clear();
+
+        counter = counter + 1;
+    }
+    println!("{}", counter);
 }
--- abc081/src/bin/c.rs
+++ abc081/src/bin/c.rs
@@ -1,3 +1,51 @@
+#[allow(unused_variables)]
 fn main() {
-    todo!();
+    let (input_n, input_k) = input_n_k();
+    let input_a = input_a();
+    let mut deduped_a: Vec<u16> = input_a.clone();
+    let mut counter_a: Vec<u16> = Vec::new();
+    let mut output_counter: u16 = 0;
+
+    deduped_a.sort();
+    deduped_a.dedup();
+
+    for filter in deduped_a.iter() {
+        counter_a.push(input_a.iter().filter(|&n| *n == *filter).count() as u16);
+    }
+    counter_a.sort();
+    counter_a.reverse();
+
+    loop {
+        if input_k < counter_a.len() as u16 {
+            output_counter = output_counter + counter_a.pop().unwrap();
+        } else {
+            break;
+        }
+    }
+
+    println!("{}", output_counter);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+fn input_n_k() -> (u16, u16) {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<u16> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return (v[0], v[1]);
+}
+
+fn input_a() -> Vec<u16> {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<u16> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return v;
 }
