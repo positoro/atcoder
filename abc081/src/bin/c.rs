189e002c4ab1c751625fb4154a700f15c0fd090d
positoro <positoro@gmail.com>
2023-04-19 21:17:00 +0900

    u32

--- abc081/src/bin/c.rs
+++ abc081/src/bin/c.rs
@@ -2,21 +2,21 @@
 fn main() {
     let (input_n, input_k) = input_n_k();
     let input_a = input_a();
-    let mut deduped_a: Vec<u16> = input_a.clone();
-    let mut counter_a: Vec<u16> = Vec::new();
-    let mut output_counter: u16 = 0;
+    let mut deduped_a: Vec<u32> = input_a.clone();
+    let mut counter_a: Vec<u32> = Vec::new();
+    let mut output_counter: u32 = 0;
 
     deduped_a.sort();
     deduped_a.dedup();
 
     for filter in deduped_a.iter() {
-        counter_a.push(input_a.iter().filter(|&n| *n == *filter).count() as u16);
+        counter_a.push(input_a.iter().filter(|&n| *n == *filter).count() as u32);
     }
     counter_a.sort();
     counter_a.reverse();
 
     loop {
-        if input_k < counter_a.len() as u16 {
+        if input_k < counter_a.len() as u32 {
             output_counter = output_counter + counter_a.pop().unwrap();
         } else {
             break;
@@ -28,10 +28,10 @@ fn main() {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-fn input_n_k() -> (u16, u16) {
+fn input_n_k() -> (u32, u32) {
     let mut input_strings = String::new();
     std::io::stdin().read_line(&mut input_strings).ok();
-    let v: Vec<u16> = input_strings
+    let v: Vec<u32> = input_strings
         .trim()
         .split_whitespace()
         .map(|e| e.parse().ok().unwrap())
@@ -39,10 +39,10 @@ fn input_n_k() -> (u16, u16) {
     return (v[0], v[1]);
 }
 
-fn input_a() -> Vec<u16> {
+fn input_a() -> Vec<u32> {
     let mut input_strings = String::new();
     std::io::stdin().read_line(&mut input_strings).ok();
-    let v: Vec<u16> = input_strings
+    let v: Vec<u32> = input_strings
         .trim()
         .split_whitespace()
         .map(|e| e.parse().ok().unwrap())
