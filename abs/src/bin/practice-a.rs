8890df1baf8a7ea8d2e95ac5c7975dd142b192d0
positoro <positoro@gmail.com>
2023-04-21 13:14:31 +0900

    practice-a

--- abs/src/bin/practice-a.rs
+++ abs/src/bin/practice-a.rs
@@ -1,3 +1,37 @@
 fn main() {
-    todo!();
+    let input_a = input_a();
+    let (input_b, input_c) = input_b_c();
+    let input_s = input_s();
+
+    println!("{:?} {}", input_a + input_b + input_c, input_s);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+fn input_b_c() -> (u16, u16) {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<u16> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return (v[0], v[1]);
+}
+
+fn input_s() -> String {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    return input_strings;
+}
+
+fn input_a() -> u16 {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<u16> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return v[0];
 }
