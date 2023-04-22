026b07be2460d00cc21a445b82479ba5719b9ce5
positoro <positoro@gmail.com>
2023-04-22 18:03:43 +0900

    abc049c

--- abs/src/bin/abc049c.rs
+++ abs/src/bin/abc049c.rs
@@ -1,3 +1,30 @@
 fn main() {
-    todo!();
+    let mut input_string = input_string();
+    if input_string.find("eraser").is_some() {
+        input_string = input_string.replace("eraser", "");
+    }
+    if input_string.find("erase").is_some() {
+        input_string = input_string.replace("erase", "");
+    }
+    if input_string.find("dreamer").is_some() {
+        input_string = input_string.replace("dreamer", "");
+    }
+    if input_string.find("dream").is_some() {
+        input_string = input_string.replace("dream", "");
+    }
+
+    let check_str = input_string.trim();
+    if check_str.is_empty() {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+fn input_string() -> String {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    input_strings
 }
