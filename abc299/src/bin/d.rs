359bdd675e7846ce33dd317900976dd103537fde
positoro <positoro@gmail.com>
2023-04-23 14:15:35 +0900

    d.rs

--- abc299/src/bin/d.rs
+++ abc299/src/bin/d.rs
@@ -1,26 +1,24 @@
 fn main() {
     let input_n = input_n();
-    let mut left = 2;
+    let mut left = 0;
     let mut right = input_n - 1;
-
-    while left < right {
+    while left + 1 < right {
         let query_index = (left + right) / 2;
         let answer = input_interactive_si(query_index);
         if answer == 1 {
             right = query_index;
         } else {
-            left = query_index + 1;
+            left = query_index;
         }
-
-        println!("{}", right - 1);
     }
+    println!("! {}", right);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 fn input_interactive_si(i: u32) -> u32 {
     let mut input_strings = String::new();
-    println!("? {}", i);
+    println!("? {}", i + 1);
     std::io::stdin().read_line(&mut input_strings).ok();
     let v: Vec<u32> = input_strings
         .trim()
