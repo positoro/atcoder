326996f98bade2a11114f68a656aea28bd46077f
positoro <positoro@gmail.com>
2023-04-24 22:31:34 +0900

    complexity

--- practice/src/bin/b.rs
+++ practice/src/bin/b.rs
@@ -1,16 +1,88 @@
-use std::io::{stdin, stdout, BufReader, Write};
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[allow(dead_code)]
 
 fn main() {
     let (input_n, input_q) = input_usizen_u16q();
     let label: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string();
     let string_for_sort: String = label[0..input_n].to_string();
-    //    let answer = quick_sort(string_for_sort);
-    let answer = merge_sort(string_for_sort);
+    let answer: String;
+    if input_n == 5 {
+        answer = five_sort(string_for_sort);
+    } else {
+        //answer = quick_sort(string_for_sort);
+        answer = merge_sort(string_for_sort);
+    }
     println!("! {}", answer);
-    stdout().flush().unwrap();
 }
 
 /////////////////////////////////////////////////////////////////////////////////
+
+fn five_sort(mut string_for_sort: String) -> String {
+    let return_string: String = String::new();
+
+    let char_4: char = string_for_sort.pop().unwrap();
+
+    let left: String = string_for_sort[0..2].to_string();
+    let right: String = string_for_sort[2..4].to_string();
+
+    let mut merged = merge_sort(left.clone()) + &merge_sort(right.clone());
+
+    let char_of_left_query = merged.chars().nth(1).unwrap();
+    let char_of_right_query = merged.chars().nth(3).unwrap();
+
+    if interactive_query(char_of_left_query, char_of_right_query) == '>' {
+        let mut merged_swap: String = String::new();
+
+        merged_swap.push(merged.chars().nth(2).unwrap());
+        merged_swap.push(merged.chars().nth(0).unwrap());
+        merged_swap.push(merged.chars().nth(1).unwrap());
+        merged_swap.push(merged.chars().nth(3).unwrap());
+        merged = merged_swap.clone();
+    } else {
+        let mut merged_swap: String = String::new();
+
+        merged_swap.push(merged.chars().nth(0).unwrap());
+        merged_swap.push(merged.chars().nth(2).unwrap());
+        merged_swap.push(merged.chars().nth(3).unwrap());
+        merged_swap.push(merged.chars().nth(1).unwrap());
+        merged = merged_swap.clone();
+    }
+
+    let char_3: char = merged.pop().unwrap();
+    let char_0: char = merged.chars().nth(0).unwrap();
+
+    let mut char_4_inputted: String = binary_search(merged.clone(), char_4);
+    let index_char_0: usize = char_4_inputted.find(char_0).unwrap();
+    let char_0_second: char = char_4_inputted.remove(index_char_0);
+    let mut char_3_inputted: String = binary_search(char_4_inputted.clone(), char_3);
+    char_3_inputted.insert(index_char_0, char_0_second);
+
+    return return_string;
+}
+
+fn binary_search(mut search_string: String, insert_char: char) -> String {
+    if search_string.len() == 0 {
+        search_string.push(insert_char);
+    } else if search_string.len() == 1 {
+        if interactive_query(search_string.chars().nth(0).unwrap(), insert_char) == '>' {
+            search_string.insert(0, insert_char);
+        } else {
+            search_string.push(insert_char);
+        }
+    } else {
+        let m = search_string.len() / 2;
+        if interactive_query(search_string.chars().nth(m).unwrap(), insert_char) == '>' {
+            search_string = binary_search(search_string[0..m].to_string(), insert_char)
+                + &search_string[m..search_string.len()].to_string();
+        } else {
+            search_string = search_string[m + 1..search_string.len()].to_string()
+                + &binary_search(search_string[0..m + 1].to_string(), insert_char);
+        }
+    }
+
+    return search_string;
+}
 
 fn merge_sort(string_for_sort: String) -> String {
     let pivot_index = string_for_sort.len() / 2;
@@ -18,7 +90,6 @@ fn merge_sort(string_for_sort: String) -> String {
     let mut right: String = string_for_sort[pivot_index..string_for_sort.len()].to_string();
 
     let mut merged: String = String::new();
-    let return_string: String = String::new();
     if string_for_sort.len() <= 1 {
         return string_for_sort;
     }
@@ -74,7 +145,6 @@ fn quick_sort(string_for_sort: String) -> String {
 fn interactive_query(c1: char, c2: char) -> char {
     let mut input_strings = String::new();
     println!("? {} {}", c1, c2);
-    stdout().flush().unwrap();
 
     std::io::stdin().read_line(&mut input_strings).ok();
     let v: Vec<char> = input_strings
