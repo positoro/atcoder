4b6edd7ef0ac74f1aec84f8efb7b7f3fa7233f3f
positoro <positoro@gmail.com>
2023-04-25 18:36:04 +0900

    inputter

--- abs/src/bin/abc086c.rs
+++ abs/src/bin/abc086c.rs
@@ -1,3 +1,37 @@
+#[allow(unused_variables)]
 fn main() {
-    todo!();
+    let input_n: u32 = input_n();
+    let input_txy_vec_vec: Vec<Vec<u32>> = input_txy_vec_vec(input_n);
+    println!("{:?}--{:?}", input_n, input_txy_vec_vec);
+}
+////////////////////////////////////////////////////////////////////////////////
+
+fn input_txy_vec_vec(n: u32) -> Vec<Vec<u32>> {
+    let mut input_strings = String::new();
+    let mut return_vec: Vec<Vec<u32>> = Vec::new();
+    for _ in 0..n {
+        std::io::stdin().read_line(&mut input_strings).ok();
+        let v: Vec<u32> = input_strings
+            .trim()
+            .split_whitespace()
+            .map(|e| e.parse().ok().unwrap())
+            .collect();
+        //        array[0] = v[0];
+        //        array[1] = v[1];
+        //        array[2] = v[2];
+        return_vec.push(v.clone());
+        input_strings.clear();
+    }
+    return return_vec;
+}
+
+fn input_n() -> u32 {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<u32> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return v[0];
 }
