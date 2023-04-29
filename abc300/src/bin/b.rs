f865dee5c921aaabaacf5361c8e6c85a7a883349
positoro <positoro@gmail.com>
2023-04-30 00:55:29 +0900

    continued

--- abc300/src/bin/b.rs
+++ abc300/src/bin/b.rs
@@ -4,23 +4,19 @@ fn main() {
 
     let count_of_a = counter(&input_a_char_vec_vec, input_h);
     let count_of_b = counter(&input_b_char_vec_vec, input_h);
-    let mut print_str: &str = "Yes";
-
-    if count_of_a != count_of_b {
-        print_str = "No";
-    }
+    let mut print_str: &str = "No";
 
     for i in 1..input_h {
         for j in 1..input_w {
             let rotated_vec_vec: Vec<Vec<char>> =
                 rotate(&input_a_char_vec_vec, i, j, input_h, input_w);
-            if check(&rotated_vec_vec, &input_b_char_vec_vec) == false {
-                print_str = "No";
-                break;
+            if check(&rotated_vec_vec, &input_b_char_vec_vec) == true {
+                println!("{:?} --- {:?}", &rotated_vec_vec, &input_b_char_vec_vec);
+                print_str = "Yes";
+                print_str = "Yes";
             }
         }
     }
-
     println!("{}", print_str);
 }
 
@@ -36,23 +32,30 @@ fn rotate(vec_vec: &Vec<Vec<char>>, i: u16, j: u16, h: u16, w: u16) -> Vec<Vec<c
             rotate_height.push(rotate_source[_h as usize].clone());
         }
         rotate_height.push(rotate_source[0 as usize].clone());
-
         rotate_source = rotate_height.clone();
-        println!("{:?}", rotate_source);
+        rotate_height.clear();
     }
     for _j in 0..j {
         for _h in 0..h {
             let mut line: Vec<char> = rotate_source[_h as usize].clone();
             let insert_char: char = line.remove(0);
             line.push(insert_char);
-            rotate_width.push(line);
+            rotate_width.push(line.clone());
         }
         rotate_source = rotate_width.clone();
+        rotate_width.clear();
     }
     return rotate_source;
 }
+
 fn check(vec_vec_a: &Vec<Vec<char>>, vec_vec_b: &Vec<Vec<char>>) -> bool {
-    let mut return_flag: bool = true;
+    let return_flag: bool;
+
+    if vec_vec_a == vec_vec_b {
+        return_flag = true;
+    } else {
+        return_flag = false;
+    }
 
     return return_flag;
 }
