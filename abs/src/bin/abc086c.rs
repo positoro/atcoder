8fbf2ddf5b0cd9b27f04030bf6d4c14f96fee6c4
positoro <positoro@gmail.com>
2023-04-29 20:19:31 +0900

    abc086

--- abs/src/bin/abc086c.rs
+++ abs/src/bin/abc086c.rs
@@ -1,24 +1,49 @@
 #[allow(unused_variables)]
 fn main() {
     let input_n: u32 = input_n();
-    let input_txy_vec_vec: Vec<Vec<u32>> = input_txy_vec_vec(input_n);
-    println!("{:?}--{:?}", input_n, input_txy_vec_vec);
+    let input_txy_vec_vec: Vec<Vec<i32>> = input_txy_vec_vec(input_n);
+    let mut previous_time: i32 = 0;
+    let mut previous_axis: [i32; 2] = [0, 0];
+    let mut ableton: &str = "Yes";
+
+    for n in 0..input_n {
+        let next_axis: [i32; 2] = [
+            input_txy_vec_vec[n as usize][1],
+            input_txy_vec_vec[n as usize][2],
+        ];
+
+        let axis_length: [i32; 2] = [
+            next_axis[0] - previous_axis[0],
+            next_axis[1] - previous_axis[1],
+        ];
+        previous_axis = next_axis;
+
+        let length: i32 = axis_length[0].abs() + axis_length[1].abs();
+
+        let time = input_txy_vec_vec[n as usize][0] - previous_time;
+        previous_time = input_txy_vec_vec[n as usize][0];
+        if length <= time && (time % length) == 0 {
+            continue;
+        } else {
+            ableton = "No";
+            break;
+        }
+    }
+
+    println!("{}", ableton);
 }
 ////////////////////////////////////////////////////////////////////////////////
 
-fn input_txy_vec_vec(n: u32) -> Vec<Vec<u32>> {
+fn input_txy_vec_vec(n: u32) -> Vec<Vec<i32>> {
     let mut input_strings = String::new();
-    let mut return_vec: Vec<Vec<u32>> = Vec::new();
+    let mut return_vec: Vec<Vec<i32>> = Vec::new();
     for _ in 0..n {
         std::io::stdin().read_line(&mut input_strings).ok();
-        let v: Vec<u32> = input_strings
+        let v: Vec<i32> = input_strings
             .trim()
             .split_whitespace()
             .map(|e| e.parse().ok().unwrap())
             .collect();
-        //        array[0] = v[0];
-        //        array[1] = v[1];
-        //        array[2] = v[2];
         return_vec.push(v.clone());
         input_strings.clear();
     }
