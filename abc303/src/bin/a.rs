071565f2c6009194adee11037853537bee04c099
positoro <positoro@gmail.com>
2023-05-27 22:37:59 +0900

    abc303

---
+++ abc303/src/bin/a.rs
@@ -0,0 +1,99 @@
+fn main() {
+    let n: u8 = input_n();
+    let s: String = input_s();
+    let t: String = input_s();
+
+    for i in 0..n as usize {
+        let s: char = s.chars().nth(i).unwrap();
+        let t: char = t.chars().nth(i).unwrap();
+
+        if (s == t
+            || (s == '1' && t == 'l')
+            || (s == 'l' && t == '1')
+            || (s == 'o' && t == '0')
+            || (s == '0' && t == 'o'))
+        {
+            continue;
+        } else {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+fn get_char_from_string_vector(h: usize, w: usize, string_vector: &Vec<String>) -> char {
+    let return_char: char = string_vector[h].chars().nth(w).unwrap();
+    return return_char;
+}
+
+fn input_string_vector(low: usize) -> Vec<String> {
+    let mut input_strings = String::new();
+    let mut return_vec: Vec<String> = Vec::new();
+
+    for _ in 0..low {
+        std::io::stdin().read_line(&mut input_strings).ok();
+        let push_low: String = input_strings.trim().parse().ok().unwrap();
+        return_vec.push(push_low);
+        input_strings.clear();
+    }
+
+    return return_vec;
+}
+
+fn input_h_w() -> (usize, usize) {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<usize> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return (v[0], v[1]);
+}
+fn stdout_i32_vector(v: &Vec<i32>) {
+    let s_vec: Vec<String> = v.iter().map(|x| x.to_string()).collect();
+    println!("{}", s_vec.join(" "));
+}
+
+fn input_s() -> String {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    input_strings.trim().parse().ok().unwrap()
+}
+
+fn input_u32_vector() -> Vec<u32> {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<u32> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return v;
+}
+
+fn input_a_b() -> (u16, u16) {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<u16> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return (v[0], v[1]);
+}
+
+fn input_n() -> u8 {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<u8> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return v[0];
+}
---
+++ abc303/src/bin/b.rs
@@ -0,0 +1,85 @@
+fn main() {
+    let (n, m) = input_n_m();
+    let a = input_u8_vector(m);
+    let mut friend_table: Vec<Vec<bool>> = vec![vec![false]; n * m];
+
+    for _m in 0..m {
+        for _n in 0..n {
+            let s: u8 = get_char_from_vec_vec(_m, _n, m, n, &a);
+            set_friend_table(n as usize, _n, s as usize, &a[m], &mut friend_table);
+        }
+    }
+
+    println!("{:?}", friend_table);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+fn set_friend_table(
+    n_max: usize,
+    n: usize,
+    person_n: usize,
+    line: &Vec<u8>,
+    table: &mut Vec<Vec<bool>>,
+) {
+    if n == 0 {
+        table[person_n][line[1] as usize] = true;
+    } else if n == n_max - 1 {
+        table[person_n][line[n_max - 2] as usize] = true;
+    } else {
+        for i in 1..n_max - 2 {
+            table[person_n][line[i - 1] as usize] = true;
+            table[person_n][line[i + 1] as usize] = true;
+        }
+    }
+
+    return;
+}
+
+fn get_char_from_vec_vec(
+    h: usize,
+    w: usize,
+    m: usize,
+    n: usize,
+    string_vector: &Vec<Vec<u8>>,
+) -> u8 {
+    let _h = h % m;
+    let _w = w % n;
+
+    let return_u8: u8 = string_vector[_h][_w];
+    return return_u8;
+}
+
+fn input_u8_vector(low: usize) -> Vec<Vec<u8>> {
+    let mut input_strings = String::new();
+    let mut return_vec: Vec<Vec<u8>> = Vec::new();
+
+    for _ in 0..low {
+        std::io::stdin().read_line(&mut input_strings).ok();
+
+        let v: Vec<u8> = input_strings
+            .trim()
+            .split_whitespace()
+            .map(|e| e.parse().ok().unwrap())
+            .collect();
+
+        return_vec.push(v);
+        input_strings.clear();
+    }
+
+    return return_vec;
+}
+
+fn input_n_m() -> (usize, usize) {
+    let mut input_strings = String::new();
+    std::io::stdin().read_line(&mut input_strings).ok();
+    let v: Vec<usize> = input_strings
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return (v[0], v[1]);
+}
+fn stdout_i32_vector(v: &Vec<i32>) {
+    let s_vec: Vec<String> = v.iter().map(|x| x.to_string()).collect();
+    println!("{}", s_vec.join(" "));
+}
