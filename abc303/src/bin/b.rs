d76c2c6d995376ef6556b5f4906d772c0f77cb82
positoro <positoro@gmail.com>
2023-06-03 18:54:13 +0900

    abc303 b

--- abc303/src/bin/b.rs
+++ abc303/src/bin/b.rs
@@ -1,19 +1,37 @@
 fn main() {
     let (n, m) = input_n_m();
     let a = input_u8_vector(m);
-    let mut friend_table: Vec<Vec<bool>> = vec![vec![false]; n * m];
+    let mut friend_table: Vec<Vec<bool>> = vec![vec![false; n]; n];
+    let mut not_friend_counts: u32 = 0;
 
     for _m in 0..m {
         for _n in 0..n {
             let s: u8 = get_char_from_vec_vec(_m, _n, m, n, &a);
-            set_friend_table(n as usize, _n, s as usize, &a[m], &mut friend_table);
+            set_friend_table(n as usize, _n, s as usize, &a[_m], &mut friend_table);
         }
     }
 
-    println!("{:?}", friend_table);
+    not_friend_counts = count_not_friend(&friend_table, n);
+
+    println!("{:?}", not_friend_counts);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+fn count_not_friend(friend_table: &Vec<Vec<bool>>, n: usize) -> u32 {
+    let mut return_count: u32 = 0;
+
+    for i in 0..n {
+        for j in 0..n {
+            if friend_table[i][j] == false {
+                return_count = return_count + 1;
+            }
+        }
+    }
+
+    return_count = (return_count - n as u32) / 2;
+    return return_count;
+}
+
 fn set_friend_table(
     n_max: usize,
     n: usize,
@@ -22,14 +40,12 @@ fn set_friend_table(
     table: &mut Vec<Vec<bool>>,
 ) {
     if n == 0 {
-        table[person_n][line[1] as usize] = true;
+        table[person_n - 1][(line[1] - 1) as usize] = true;
     } else if n == n_max - 1 {
-        table[person_n][line[n_max - 2] as usize] = true;
+        table[person_n - 1][(line[n_max - 2] - 1) as usize] = true;
     } else {
-        for i in 1..n_max - 2 {
-            table[person_n][line[i - 1] as usize] = true;
-            table[person_n][line[i + 1] as usize] = true;
-        }
+        table[person_n - 1][(line[n - 1] - 1) as usize] = true;
+        table[person_n - 1][(line[n + 1] - 1) as usize] = true;
     }
 
     return;
