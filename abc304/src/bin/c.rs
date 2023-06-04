94672cff8c24e9dac10fd84be739b1f4acb3924a
positoro <positoro@gmail.com>
2023-06-04 20:33:59 +0900

    abc304 c

--- abc304/src/bin/c.rs
+++ abc304/src/bin/c.rs
@@ -5,18 +5,12 @@ fn main() {
 
     infected_vec[0] = true;
     for i in 0..input_n as usize {
-        for j in 0..input_n as usize {
-            if infected_vec[i] == true {
-                if (input_xy_vec[i].0 - input_xy_vec[j].0) * (input_xy_vec[i].0 - input_xy_vec[j].0)
-                    + (input_xy_vec[i].1 - input_xy_vec[j].1)
-                        * (input_xy_vec[i].1 - input_xy_vec[j].1)
-                    <= input_d * input_d
-                {
-                    infected_vec[j] = true;
-                }
-            }
+        if infected_vec[i] == true {
+            infect(input_n, input_d, i, &mut infected_vec, &input_xy_vec);
         }
+    }
 
+    for i in 0..input_n as usize {
         if infected_vec[i] == true {
             println!("Yes");
         } else {
@@ -26,6 +20,31 @@ fn main() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+
+fn infect(
+    n: i32,
+    d: i32,
+    infect_index: usize,
+    infected_vec: &mut Vec<bool>,
+    input_xy_vec: &Vec<(i32, i32)>,
+) {
+    infected_vec[infect_index] = true;
+
+    for i in 0..n as usize {
+        for j in 0..n as usize {
+            if infected_vec[i] == true {
+                if (input_xy_vec[i].0 - input_xy_vec[j].0) * (input_xy_vec[i].0 - input_xy_vec[j].0)
+                    + (input_xy_vec[i].1 - input_xy_vec[j].1)
+                        * (input_xy_vec[i].1 - input_xy_vec[j].1)
+                    <= d * d
+                {
+                    infected_vec[j] = true;
+                    infect(n, d, j, infected_vec, input_xy_vec);
+                }
+            }
+        }
+    }
+}
 
 fn input_n_d() -> (i32, i32) {
     let mut input_strings = String::new();
