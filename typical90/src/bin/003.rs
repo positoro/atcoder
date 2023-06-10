17dcf321d7273d9c1462bd6d13da8a9fd562eb02
positoro <positoro@gmail.com>
2023-06-10 20:59:07 +0900

    003 continue

--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -3,26 +3,33 @@ fn main() {
     let a_b_tuple_vector: Vec<(u32, u32)> = input_u32_tuple_vector(n);
     let tuple_vector: Vec<(u32, u32)> = set_tuple_vector_zero_index(a_b_tuple_vector);
     let graph = create_graph(n, tuple_vector);
-    let distance_from_zero_vector: Vec<u32> = dfs(&graph, 0);
+    let distance_from_zero_vector: Vec<i32> = dfs(&graph, 0);
 
-    let (max_distance_node_from_zero, max_distance_from_zero): (u32, u32) =
+    let (max_distance_node_from_zero, max_distance_from_zero): (i32, i32) =
         get_max_distance_node(distance_from_zero_vector);
 
-    let distance_from_max_distance_vector: Vec<u32> = dfs(&graph, max_distance_node_from_zero);
-    let (max_distance_node_from_max_distance, max_distance_from_max_distance): (u32, u32) =
+    let distance_from_max_distance_vector: Vec<i32> = dfs(&graph, max_distance_node_from_zero);
+    let (max_distance_node_from_max_distance, max_distance_from_max_distance): (i32, i32) =
         get_max_distance_node(distance_from_max_distance_vector);
 
     println!("{}", max_distance_from_max_distance);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-fn dfs(graph: &Vec<Vec<u32>>, from_node: u32) -> Vec<u32> {
-    let return_vector: Vec<u32> = Vec::new();
+fn dfs(graph: &Vec<Vec<u32>>, from_node: i32) -> Vec<i32> {
+    let graph_length: usize = graph.len();
+    let return_vector: Vec<i32> = vec![-1; graph_length];
+    let mut stack: Vec<i32> = vec![from_node];
+
+    while stack.is_empty() == false {
+        let mut v = stack.pop().unwrap();
+        for (index, value) in graph[v as usize].iter().enumerate() {}
+    }
     return return_vector;
 }
-fn get_max_distance_node(distance_vector: Vec<u32>) -> (u32, u32) {
+fn get_max_distance_node(distance_vector: Vec<i32>) -> (i32, i32) {
     let mut max_node_index: i32 = -1;
-    let mut max_distance: u32 = 0;
+    let mut max_distance: i32 = -1;
 
     for (index, value) in distance_vector.iter().enumerate() {
         if max_distance < *value {
@@ -30,7 +37,7 @@ fn get_max_distance_node(distance_vector: Vec<u32>) -> (u32, u32) {
             max_node_index = index as i32;
         }
     }
-    return (max_node_index as u32, max_distance);
+    return (max_node_index, max_distance);
 }
 
 fn set_tuple_vector_zero_index(a_b_tuple_vector: Vec<(u32, u32)>) -> Vec<(u32, u32)> {
