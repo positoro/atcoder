2813796583c5157ff67c425dc624109fac15b5f3
positoro <positoro@gmail.com>
2023-06-11 17:02:47 +0900

    clean

--- abc305/src/bin/c.rs
+++ abc305/src/bin/c.rs
@@ -1,45 +1,32 @@
 fn main() {
-    let input_u32_h_w = input_u32_tuple();
-    let input_s = input_string_vector(input_u32_h_w.0 as usize);
-    let mut max_h = 0;
-    let mut min_h = input_u32_h_w.0;
-    let mut max_w = 0;
-    let mut min_w = input_u32_h_w.1;
+    let input_u32_n_l = input_u32_tuple();
+    let input_u32_k = input_u32();
+    let input_u32_vec_a = input_u32_vector();
 
-    for h in 0..input_u32_h_w.0 {
-        for w in 0..input_u32_h_w.1 {
-            if get_char_from_string_vector(h as usize, w as usize, &input_s) == '#' {
-                if max_h < h {
-                    max_h = h;
-                }
-                if max_w < w {
-                    max_w = w;
-                }
-                if min_h > h {
-                    min_h = h;
-                }
-                if min_w > w {
-                    min_w = w;
-                }
-            }
-        }
-    }
-
-    let mut return_h_w: (u32, u32) = (0, 0);
-    for h in 0..input_u32_h_w.0 {
-        for w in 0..input_u32_h_w.1 {
-            if max_h >= h && min_h <= h && max_w >= w && min_w <= w {
-                if get_char_from_string_vector(h as usize, w as usize, &input_s) == '.' {
-                    return_h_w = (h, w);
-                }
-            }
-        }
-    }
-
-    println!("{} {}", return_h_w.0 + 1, return_h_w.1 + 1);
+    println!("{:?}", input_u32_n_l);
+    println!("{:?}", input_u32_k);
+    println!("{:?}", input_u32_vec_a);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+
+fn input_u32_tuple_vector(n: u32) -> Vec<(u32, u32)> {
+    let mut input_strings = String::new();
+    let mut return_vector = Vec::new();
+
+    for _ in 0..(n - 1) {
+        std::io::stdin().read_line(&mut input_strings).ok();
+        let v: Vec<u32> = input_strings
+            .trim()
+            .split_whitespace()
+            .map(|e| e.parse().ok().unwrap())
+            .collect();
+        input_strings.clear();
+        return_vector.push((v[0], v[1]));
+    }
+
+    return return_vector;
+}
 
 fn get_char_from_string_vector(h: usize, w: usize, string_vector: &Vec<String>) -> char {
     let return_char: char = string_vector[h].chars().nth(w).unwrap();
