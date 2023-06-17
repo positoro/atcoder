0cacde5c0d5add03bc04ee261933785b9b17f7dc
positoro <positoro@gmail.com>
2023-06-17 23:01:27 +0900

    c

--- abc306/src/bin/c.rs
+++ abc306/src/bin/c.rs
@@ -1,9 +1,9 @@
-use ascii::AsciiChar::v;
+use std::collections::HashMap;
 
 fn main() {
     let n_u32 = input_u32();
     let a_u32_vec = input_u32_vector();
-    let mut f_vec = Vec::new();
+    let mut scores = HashMap::new();
 
     for i_n in 0..n_u32 {
         let mut counter: u32 = 0;
@@ -11,25 +11,30 @@ fn main() {
             if a_u32_vec[i_a as usize] == (i_n + 1) {
                 counter = counter + 1;
                 if counter == 2 {
-                    f_vec.push(i_a as u32);
+                    scores.insert(i_n, i_a);
                     break;
                 }
             }
         }
     }
-    let idx = argsort(&f_vec);
 
-    for i in idx.into_iter() {
-        println!("v[{}] = {}", i, v[i]);
+    let mut vec: Vec<(&u32, &u32)> = scores.iter().collect();
+    vec.sort_by(|a, b| a.1.cmp(&b.1));
+
+    let mut vec2: Vec<i32> = Vec::new();
+    for i in vec.iter() {
+        vec2.push((i.0 + 1) as i32);
     }
+    stdout_i32_vector(&vec2);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-pub fn argsort<T: Ord>(data: &[T]) -> Vec<usize> {
-    let mut indices = (0..data.len()).collect::<Vec<_>>();
-    indices.sort_by_key(|&i| &data[i]);
-    indices
+
+fn stdout_i32_vector(v: &Vec<i32>) {
+    let s_vec: Vec<String> = v.iter().map(|x| x.to_string()).collect();
+    println!("{}", s_vec.join(" "));
 }
+
 fn input_u32_vector() -> Vec<u32> {
     let mut input_strings = String::new();
     std::io::stdin().read_line(&mut input_strings).ok();
