ea2cdeba67fd2cf655a9129c5164e92c0f3a7134
positoro <positoro@gmail.com>
2023-07-12 16:10:52 +0900

    continue

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,28 +1,12 @@
 fn main() {
-    let input_n = input_n() as usize;
-    let mut input_u32_vector = input_u32_vector();
+    let h_w = input_h_w();
+    let a_u32_vec_vec = input_u32_vector_vector(h_w.0);
+
+    println!("{:?}", h_w);
+    println!("{:?}", a_u32_vec_vec);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-
-fn get_char_from_string_vector(h: usize, w: usize, string_vector: &Vec<String>) -> char {
-    let return_char: char = string_vector[h].chars().nth(w).unwrap();
-    return return_char;
-}
-
-fn input_string_vector(low: usize) -> Vec<String> {
-    let mut input_strings = String::new();
-    let mut return_vec: Vec<String> = Vec::new();
-
-    for _ in 0..low {
-        std::io::stdin().read_line(&mut input_strings).ok();
-        let push_low: String = input_strings.trim().parse().ok().unwrap();
-        return_vec.push(push_low);
-        input_strings.clear();
-    }
-
-    return return_vec;
-}
 
 fn input_h_w() -> (usize, usize) {
     let mut input_strings = String::new();
@@ -34,46 +18,20 @@ fn input_h_w() -> (usize, usize) {
         .collect();
     return (v[0], v[1]);
 }
-fn stdout_i32_vector(v: &Vec<i32>) {
-    let s_vec: Vec<String> = v.iter().map(|x| x.to_string()).collect();
-    println!("{}", s_vec.join(" "));
-}
 
-fn input_s() -> String {
+fn input_u32_vector_vector(low: usize) -> Vec<Vec<u32>> {
     let mut input_strings = String::new();
-    std::io::stdin().read_line(&mut input_strings).ok();
-    input_strings.trim().parse().ok().unwrap()
-}
+    let mut return_u32_vector_vector = Vec::new();
+    for _ in 0..low {
+        std::io::stdin().read_line(&mut input_strings).ok();
+        let v: Vec<u32> = input_strings
+            .trim()
+            .split_whitespace()
+            .map(|e| e.parse().ok().unwrap())
+            .collect();
+        input_strings.clear();
+        return_u32_vector_vector.push(v);
+    }
 
-fn input_u32_vector() -> Vec<u32> {
-    let mut input_strings = String::new();
-    std::io::stdin().read_line(&mut input_strings).ok();
-    let v: Vec<u32> = input_strings
-        .trim()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect();
-    return v;
-}
-
-fn input_a_b() -> (u16, u16) {
-    let mut input_strings = String::new();
-    std::io::stdin().read_line(&mut input_strings).ok();
-    let v: Vec<u16> = input_strings
-        .trim()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect();
-    return (v[0], v[1]);
-}
-
-fn input_n() -> u8 {
-    let mut input_strings = String::new();
-    std::io::stdin().read_line(&mut input_strings).ok();
-    let v: Vec<u8> = input_strings
-        .trim()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect();
-    return v[0];
+    return return_u32_vector_vector;
 }
