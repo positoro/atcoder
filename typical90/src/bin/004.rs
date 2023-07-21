7afc2beb8b6ea9db5533217d0bb4c36b44f87670
positoro <positoro@gmail.com>
2023-07-21 19:20:46 +0900

    done?

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,14 +1,45 @@
 fn main() {
-    let h_w = input_h_w();
-    let a_u32_vec_vec = input_u32_vector_vector(h_w.0);
+    let h_w: (usize, usize) = input_tuple_usize();
+    let a_u32_vec_vec: Vec<Vec<u32>> = input_u32_vector_vector(h_w.0);
+    let mut ans_u32_vec_vec: Vec<Vec<u32>> = vec![vec![0; h_w.1]; h_w.0];
 
-    println!("{:?}", h_w);
-    println!("{:?}", a_u32_vec_vec);
+    for h in 0..h_w.0 {
+        for w in 0..h_w.1 {
+            ans_u32_vec_vec[h][w] =
+                sum_h(&a_u32_vec_vec, h) + sum_w(&a_u32_vec_vec, w) - a_u32_vec_vec[h][w];
+        }
+    }
+    stdout_vector_vector_u32(&ans_u32_vec_vec);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-fn input_h_w() -> (usize, usize) {
+fn sum_w(a: &Vec<Vec<u32>>, w: usize) -> u32 {
+    let mut return_sum = 0;
+    for h in a.iter() {
+        return_sum = return_sum + h[w];
+    }
+    return return_sum;
+}
+
+fn sum_h(a: &Vec<Vec<u32>>, h: usize) -> u32 {
+    let mut return_sum = 0;
+    for w in a[h].iter() {
+        return_sum = return_sum + w;
+    }
+    return return_sum;
+}
+
+fn stdout_vector_vector_u32(v: &Vec<Vec<u32>>) {
+    for h in v.iter() {
+        for w in h.iter() {
+            print!("{} ", w);
+        }
+        println!();
+    }
+}
+
+fn input_tuple_usize() -> (usize, usize) {
     let mut input_strings = String::new();
     std::io::stdin().read_line(&mut input_strings).ok();
     let v: Vec<usize> = input_strings
