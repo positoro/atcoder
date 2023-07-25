ae6f1fad33d3a8a77b0fd5b5b00e081412366847
positoro <positoro@gmail.com>
2023-07-25 18:15:19 +0900

    passed

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -3,11 +3,15 @@ fn main() {
     let a_vec_u32: Vec<u32> = input_vector_u32();
     let q_u32: u32 = input_u32();
     let b_vec_u32: Vec<u32> = input_vector_u32_low(&q_u32);
+    let mut complain_rate: Vec<u32> = Vec::new();
 
-    println!("{:?}", n_u32);
-    println!("{:?}", a_vec_u32);
-    println!("{:?}", q_u32);
-    println!("{:?}", b_vec_u32);
+    for q in 0..q_u32 {
+        for n in 0..n_u32 {
+            complain_rate.push((a_vec_u32[n as usize]).abs_diff(b_vec_u32[q as usize]));
+        }
+        println!("{}", complain_rate.iter().min().unwrap());
+        complain_rate.clear();
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -36,6 +40,7 @@ fn input_vector_u32() -> Vec<u32> {
 
 fn input_vector_u32_low(low: &u32) -> Vec<u32> {
     let mut return_vector: Vec<u32> = Vec::new();
+
     for _ in 0..*low {
         return_vector.push(input_u32());
     }
