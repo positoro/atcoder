f3eef864b3270e3444fd847c6e46f9bfa86c7e14
positoro <positoro@gmail.com>
2023-07-27 15:09:51 +0900

    cumulative sum

--- typical90/src/bin/010.rs
+++ typical90/src/bin/010.rs
@@ -4,17 +4,32 @@ fn main() {
     let q_u32: u32 = input_u32();
     let l_r_vector_tuple: Vec<(u32, u32)> = input_tuple_low(&q_u32);
 
-    for q in 0..q_u32 {
-        let mut class_one_point: u32 = 0;
-        let mut class_two_point: u32 = 0;
+    let mut class_one_cumulative_point_vec: Vec<u32> = vec![0; (n_u32 + 1) as usize];
+    let mut class_two_cumulative_point_vec: Vec<u32> = vec![0; (n_u32 + 1) as usize];
+    let mut class_one_cumulative_point: u32 = 0;
+    let mut class_two_cumulative_point: u32 = 0;
 
-        for i in (l_r_vector_tuple[q as usize].0) - 1..l_r_vector_tuple[q as usize].1 {
-            if c_p_vector_tuple[i as usize].0 == 1 {
-                class_one_point = class_one_point + c_p_vector_tuple[i as usize].1;
-            } else {
-                class_two_point = class_two_point + c_p_vector_tuple[i as usize].1;
-            }
+    for i in 1..n_u32 + 1 {
+        if c_p_vector_tuple[(i - 1) as usize].0 == 1 {
+            class_one_cumulative_point =
+                class_one_cumulative_point + c_p_vector_tuple[(i - 1) as usize].1;
+        } else {
+            class_two_cumulative_point =
+                class_two_cumulative_point + c_p_vector_tuple[(i - 1) as usize].1;
         }
+        class_one_cumulative_point_vec[i as usize] = class_one_cumulative_point;
+        class_two_cumulative_point_vec[i as usize] = class_two_cumulative_point;
+    }
+
+    //    println!("{:?}", class_two_cumulative_point_vec);
+    for q in 0..q_u32 {
+        let class_one_point: u32 = class_one_cumulative_point_vec
+            [(l_r_vector_tuple[q as usize].1) as usize]
+            - class_one_cumulative_point_vec[(l_r_vector_tuple[q as usize].0 - 1) as usize];
+        let class_two_point: u32 = class_two_cumulative_point_vec
+            [(l_r_vector_tuple[q as usize].1) as usize]
+            - class_two_cumulative_point_vec[(l_r_vector_tuple[q as usize].0 - 1) as usize];
+
         println!("{} {}", class_one_point, class_two_point);
     }
 }
