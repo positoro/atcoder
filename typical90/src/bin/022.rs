015868bf4b6bd7ffe22eec6f515f0542175388ac
positoro <positoro@gmail.com>
2023-07-28 18:01:45 +0900

    continue

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,79 +1,41 @@
 fn main() {
-    let input_n = input_n() as usize;
-    let mut input_u32_vector = input_u32_vector();
+    let a_b_c_tuple: (u64, u64, u64) = input_tuple();
+    let gcm: u64 = get_greatest_common_divisor(a_b_c_tuple.0, a_b_c_tuple.1, a_b_c_tuple.2);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+fn get_greatest_common_divisor(a: u64, b: u64, c: u64) -> u64 {}
 
-fn get_char_from_string_vector(h: usize, w: usize, string_vector: &Vec<String>) -> char {
-    let return_char: char = string_vector[h].chars().nth(w).unwrap();
-    return return_char;
-}
-
-fn input_string_vector(low: usize) -> Vec<String> {
-    let mut input_strings = String::new();
-    let mut return_vec: Vec<String> = Vec::new();
-
-    for _ in 0..low {
-        std::io::stdin().read_line(&mut input_strings).ok();
-        let push_low: String = input_strings.trim().parse().ok().unwrap();
-        return_vec.push(push_low);
-        input_strings.clear();
+fn _binary_search_check_satisfaction(vector: &Vec<u32>, index: usize, value: u32) -> bool {
+    if value <= vector[index] {
+        return true;
+    } else {
+        return false;
     }
-
-    return return_vec;
 }
 
-fn input_h_w() -> (usize, usize) {
+fn binary_search(a: &Vec<u32>, b: u32) -> usize {
+    let mut left: i32 = -1;
+    let mut right: i32 = a.len() as i32;
+
+    while (left + 1) < right {
+        let mid: i32 = (left + right) / 2;
+        if _binary_search_check_satisfaction(&a, mid as usize, b) == true {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+    return right as usize;
+}
+
+fn input_tuple() -> (u64, u64, u64) {
     let mut input_strings = String::new();
     std::io::stdin().read_line(&mut input_strings).ok();
-    let v: Vec<usize> = input_strings
+    let v: Vec<u64> = input_strings
         .trim()
         .split_whitespace()
         .map(|e| e.parse().ok().unwrap())
         .collect();
-    return (v[0], v[1]);
-}
-fn stdout_i32_vector(v: &Vec<i32>) {
-    let s_vec: Vec<String> = v.iter().map(|x| x.to_string()).collect();
-    println!("{}", s_vec.join(" "));
-}
-
-fn input_s() -> String {
-    let mut input_strings = String::new();
-    std::io::stdin().read_line(&mut input_strings).ok();
-    input_strings.trim().parse().ok().unwrap()
-}
-
-fn input_u32_vector() -> Vec<u32> {
-    let mut input_strings = String::new();
-    std::io::stdin().read_line(&mut input_strings).ok();
-    let v: Vec<u32> = input_strings
-        .trim()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect();
-    return v;
-}
-
-fn input_a_b() -> (u16, u16) {
-    let mut input_strings = String::new();
-    std::io::stdin().read_line(&mut input_strings).ok();
-    let v: Vec<u16> = input_strings
-        .trim()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect();
-    return (v[0], v[1]);
-}
-
-fn input_n() -> u8 {
-    let mut input_strings = String::new();
-    std::io::stdin().read_line(&mut input_strings).ok();
-    let v: Vec<u8> = input_strings
-        .trim()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect();
-    return v[0];
+    return (v[0], v[1], v[2]);
 }
