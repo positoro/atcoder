348fc23e5de97f2fc5fdd1714bd1c7c51178f561
positoro <positoro@gmail.com>
2023-07-31 03:07:14 +0900

    continue

--- abc312/src/bin/c.rs
+++ abc312/src/bin/c.rs
@@ -7,37 +7,44 @@ fn main() {
     a_vec_u32.sort();
     a_vec_u32.reverse();
     b_vec_u32.sort();
-    b_vec_u32.reverse();
 
     let max_price: u32 = *max(a_vec_u32.first().unwrap(), b_vec_u32.first().unwrap());
 
     let mut a_persons_vec: Vec<u32> = vec![0; (max_price + 1) as usize];
     let mut a_persons: u32 = 0;
     let mut b_persons_vec: Vec<u32> = vec![0; (max_price + 1) as usize];
-    let mut b_persons: u32 = 0;
+    let mut b_persons: u32 = b_vec_u32.len() as u32;
 
-    let mut a: u32 = *a_vec_u32.last().unwrap();
-    let mut b: u32 = *b_vec_u32.last().unwrap();
+    let mut a: u32 = a_vec_u32.pop().unwrap();
+    let mut b: u32 = b_vec_u32.pop().unwrap();
     for i in 1..max_price + 1 {
-        print!("i:{:?}", i);
-        while a <= i && a_vec_u32.len() > 0 {
-            a = a_vec_u32.pop().unwrap();
-            println!("a:{:?}", a);
+        while a == i {
+            if a_vec_u32.len() > 0 {
+                a = a_vec_u32.pop().unwrap();
+            } else {
+                a = 1000000000 + 1;
+            }
             a_persons = a_persons + 1;
         }
-        while b <= i && b_vec_u32.len() > 0 {
-            b = b_vec_u32.pop().unwrap();
-            b = b_vec_u32.pop().unwrap();
-            b_persons = b_persons + 1;
-            println!("i:{:?}, persons_b {}", i, b_persons);
-        }
+
         a_persons_vec[i as usize] = a_persons;
+    }
+
+    for i in (1..max_price + 1).rev() {
+        println!("{:?}", i);
+        while b == i {
+            if b_vec_u32.len() > 0 {
+                b = b_vec_u32.pop().unwrap();
+            } else {
+                b = 0;
+            }
+            b_persons = b_persons - 1;
+        }
         b_persons_vec[i as usize] = b_persons;
     }
-    println!("{:?}", a_persons_vec);
-    println!("{:?}", b_persons_vec);
-}
 
+    println!("{:?} {:?}", a_persons_vec, b_persons_vec);
+}
 ////////////////////////////////////////////////////////////////////////////////
 
 fn input_tuple() -> (u32, u32) {
