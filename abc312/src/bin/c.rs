2ec8d3f55f5e4709f151e74dd74af7abb7488cc8
positoro <positoro@gmail.com>
2023-07-31 13:39:15 +0900

    continue

--- abc312/src/bin/c.rs
+++ abc312/src/bin/c.rs
@@ -8,17 +8,17 @@ fn main() {
     a_vec_u32.reverse();
     b_vec_u32.sort();
 
-    let max_price: u32 = *max(a_vec_u32.first().unwrap(), b_vec_u32.first().unwrap());
+    let max_price: u32 = *max(a_vec_u32.first().unwrap(), b_vec_u32.last().unwrap());
 
     let mut a_persons_vec: Vec<u32> = vec![0; (max_price + 1) as usize];
     let mut a_persons: u32 = 0;
     let mut b_persons_vec: Vec<u32> = vec![0; (max_price + 1) as usize];
-    let mut b_persons: u32 = b_vec_u32.len() as u32;
+    let mut b_persons: u32 = 0;
 
     let mut a: u32 = a_vec_u32.pop().unwrap();
     let mut b: u32 = b_vec_u32.pop().unwrap();
     for i in 1..max_price + 1 {
-        while a == i {
+        while a <= i {
             if a_vec_u32.len() > 0 {
                 a = a_vec_u32.pop().unwrap();
             } else {
@@ -31,19 +31,23 @@ fn main() {
     }
 
     for i in (1..max_price + 1).rev() {
-        println!("{:?}", i);
-        while b == i {
+        while b >= i {
             if b_vec_u32.len() > 0 {
                 b = b_vec_u32.pop().unwrap();
             } else {
                 b = 0;
             }
-            b_persons = b_persons - 1;
+            b_persons = b_persons + 1;
         }
         b_persons_vec[i as usize] = b_persons;
     }
 
-    println!("{:?} {:?}", a_persons_vec, b_persons_vec);
+    for i in 1..max_price + 1 {
+        if b_persons_vec[i as usize] <= a_persons_vec[i as usize] {
+            println!("{}", i);
+            break;
+        }
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////
 
